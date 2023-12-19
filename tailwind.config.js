12d2b03ace133f6e4ac86c0a05c94d1f85f512f9
AnnaDoi <willow6510@yahoo.co.jp>
2023-12-19 21:07:17 +0900

    preline導入、roomコントローラー作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,12 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    'node_modules/preline/dist/*.js',
   ],
   plugins: [
-    require('daisyui')
+    require('daisyui'),
+    require('@tailwindcss/forms'),
+    require('preline/plugin'),
   ]
 }
