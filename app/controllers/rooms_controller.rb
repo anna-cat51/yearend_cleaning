9bccaa5410a434bd2d6334e630d7e5452306301c
AnnaDoi <willow6510@yahoo.co.jp>
2023-12-20 17:19:38 +0900

    部屋追加機能実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,3 +1,24 @@
 class RoomsController < ApplicationController
-  def index;end
+  def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      flash.now.notice = "部屋を追加しました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "tops#index"
   get "/rooms", to: "rooms#index"
+  resources :rooms
 end
