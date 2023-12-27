d306fec5e53aea2e2d1d0e14d72ea5024e705dc2
AnnaDoi <willow6510@yahoo.co.jp>
2023-12-28 06:24:41 +0900

    すべてチェックでモーダルを出す機能

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,30 +1,19 @@
-import { Controller } from "stimulus";
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["checkbox"];
 
-  connect() {
-    this.checkAllChecked();
-  }
-
   checkAllChecked() {
-    const allChecked = this.checkboxTargets.every(checkbox => checkbox.checked);
+    const allChecked = this.checkboxTargets.every(
+      (checkbox) => checkbox.checked
+    );
     if (allChecked) {
-      // 直接モーダルを表示する
-      $('#congratulationsModal').modal('show');
+      this.showModal();
     }
   }
 
-
   showModal() {
-    // モーダルを表示するためのコード
-    const modal = document.getElementById("myModal");
-    modal.style.display = "block";
-    
-    // モーダルを閉じるボタン
-    const closeModalButton = document.getElementById("closeModal");
-    closeModalButton.addEventListener("click", () => {
-      modal.style.display = "none";
-    });
+    const modal = document.getElementById("my_modal_1");
+    modal.showModal(); // モーダルを表示
   }
 }
